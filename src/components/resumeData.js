9833055a36be2ca4ff86d975ef6ae73bb144b0c9
zunaia119 <zunaira0107@gmail.com>
2021-02-03 17:37:28 +0500

    first commit

---
+++ src/components/resumeData.js
@@ -0,0 +1,122 @@
+let resumeData = {
+    "imagebaseurl": "https://ivy555.github.io/",
+    "name": "Zunaira javed",
+    "role": "Full Stack Developer",
+    "linkedinId": "https://www.linkedin.com/in/zunaira-javed-5576bb98/",
+    "skypeid": "live:zunaira0107",
+    "roleDescription": "I like to learn and create applications in various parts of software development and like to learn about new technologies.",
+    "socialLinks": [
+        {
+            "name": "linkedin",
+            "url": "https://www.linkedin.com/in/zunaira-javed-5576bb98/",
+            "className": "fa fa-linkedin"
+        },
+        {
+            "name": "github",
+            "url": "https://github.com/zunaira119",
+            "className": "fa fa-github"
+        },
+        {
+            "name": "facebook",
+            "url": "",
+            "className": "fa fa-facebook"
+        }
+    ],
+    "aboutme": "I am computer science graduate with more than 2 years of experience in software development. I am a talented, ambitious and hardworking individual, with broad skills and experience in software development.\n" +
+        "\n" +
+        "Furthermore, I am adept at handling multiple tasks on a daily basis competently and at working well under pressure.\n" +
+        "\n" +
+        "A key strength is communication; building strong relationships with people in order to deliver the best results.\n" +
+        "\nI believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "address": "Pakistan",
+    "website": "",
+    "education": [
+        {
+            "UniversityName": "University Of Central Punjab",
+            "specialization": "Computer Science",
+            "MonthOfPassing": "2018",
+        },
+        {
+            "UniversityName": "Punjab College Of Science ",
+            "specialization": "Pre Engineering",
+            "MonthOfPassing": "2013",
+        },
+    ],
+    "work": [
+        {
+            "CompanyName": "Ct Ptoducts pvt ltd",
+            "specialization": "Php Laravel developer",
+            "MonthOfJoining": "Jan 2019",
+            "MonthOfLeaving": "Dec 2019",
+            "Achievements": ""
+        },
+        {
+            "CompanyName": "Programmers Force",
+            "specialization": "Php Laravel developer",
+            "MonthOfJoining": "March 2020",
+            "MonthOfLeaving": "Nov 2020",
+            "Achievements": ""
+        },
+        {
+            "CompanyName": "Suave Solutions",
+            "specialization": "Php Laravel developer",
+            "MonthOfJoining": "Nov 2020",
+            "MonthOfLeaving": "Continue",
+            "Achievements": ""
+        },
+    ],
+    "skillsDescription": "Your skills here",
+    "skills": [
+        {
+            "skillname": "PHP"
+        },
+        {
+            "skillname": "Laravel"
+        },
+        {
+            "skillname": "Postgres"
+        },
+        {
+            "skillname": "Mongodb"
+        },
+        {
+            "skillname": "MySql"
+        },
+        {
+            "skillname": "Javascript"
+        },
+        {
+            "skillname": "Html"
+        },
+        {
+            "skillname": "CSS"
+        },
+        {
+            "skillname": "Git"
+        },
+        {
+            "skillname": "Reactjs"
+        },
+
+
+    ],
+    "portfolio": [
+        {
+            "name": "Islamic Honey",
+            "description": "Ecommerce website",
+            "imgurl": "images/portfolio/project3.jpg"
+        },
+        {
+            "name": "Shuftipro",
+            "description": "Identity verification",
+            "imgurl": "images/portfolio/project3.jpg"
+        },
+        {
+            "name": "پنجاب لوک سجاگ",
+            "description": "Content Management system",
+            "imgurl": "images/portfolio/project3.jpg"
+        },
+
+    ]
+}
+export default resumeData
